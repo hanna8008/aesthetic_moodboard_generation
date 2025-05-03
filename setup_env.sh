12b6104573a26c1cd2fd193f0b5269dcbcc699ef
Hanna Zelis <hannazelis@gmail.com>
2025-05-03 18:50:46 -0500

    multiple bugs setting up envrionment and jobs but think i finally got the training running, so envrionment setup is done

--- setup_env.sh
+++ setup_env.sh
@@ -2,21 +2,21 @@
 
 echo "Setting up CVAE Moodboard Envrionment..."
 
+ENV_NAME = "moodgen"
+
 #1. Create Virtual Envrionment
-echo "Creating Virtual Envrionment..."
-python3 -m venv venv
+echo "Creating Conda Envrionment: $ENV_NAME..."
+conda create -n $ENV_NAME python=3.9 -y
 
 #2. Activate Envrionment
 echo "Activating Envrionment..."
-source venv/bin/activate
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate $ENV_NAME
 
-#3. Upgrade pip
-pip install --upgrade pip
-
-#4. Install Requirements
+#3. Install Requirements
 echo "Installing Python Packages from requirements.txt..."
 pip install -r requirements.txt
 
-#5. Success Message
-echo "Envrionment setup complete!"
-echo "To activate it later, run: source venv/bin/activate"
+#4. Success Message
+echo "Envrionment '$ENV_NAME' setup complete!"
+echo "To activate it later, run: conda activate moodgen"
--- train_cvae.sh
+++ train_cvae.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --account=p32562
+#SBATCH --account=e32706
 #SBATCH --job-name=train_cvae
 #SBATCH --output=outputs/logs/train_%j.log
 #SBATCH --time=1:00:00
@@ -8,9 +8,25 @@
 #SBATCH --mem=16G
 #SBATCH --cpus-per-task=4
 
+echo "Starting .sh file"
+
 #Activate conda or virtualenv here
-source ~/miniconda3/etc/profile.d/conda.sh
-conda activate cvae_moodboard_env
+module purge
+
+#Use hook + absolute path
+eval "$(/home/qlh2976/miniconda/bin/conda shell.bash hook)"
+conda activate /home/qlh2976/miniconda/envs/moodgen
+
+cd /home/qlh2987/spring2025/genAI/aesthetic_moodboard_generation
+
+echo "Environment activated. Starting training..."
+
+#Make sure current directory is treated as module root
+export PYTHONPATH=$(pwd)
 
 #Run training script
+echo "Starting Python training script..."
 python scripts/train_cvae.py
+
+#End
+echo "Training complete!"
