198d2365617c53e95029fa5baa65eb3697cb1614
Hanna Zelis <hannazelis@gmail.com>
2025-05-07 15:38:10 -0500

    update train_cvae.py

--- train_cvae.sh
+++ train_cvae.sh
@@ -1,39 +1,69 @@
 #!/bin/bash
+
+# -----------------------------------------------------------
+# SLURM Submission Script for CVAE Training
+# -----------------------------------------------------------
+#
+# This script is intended to run a CVAE training job on Quest's GPU
+# nodes using the A100 GPU. It sets up the environment, activates the 
+# Conda envrionment, and executes the training script.
+
+
+#project account for resource tracking
 #SBATCH --account=e32706
+#job name displayed in SLURM queue
 #SBATCH --job-name=train_cvae
+#log file output (%j = job ID)
 #SBATCH --output=outputs/logs/train_%j.log
+#max wall time for the job
 #SBATCH --time=48:00:00
+#GPU partition to use
 #SBATCH --partition=gengpu
+#reuqest 1 A100 GPU
 #SBATCH --gres=gpu:a100:1
+#total memory required
 #SBATCH --mem=40G
+#number of CPU threads
 #SBATCH --cpus-per-task=2
 
-echo "Starting .sh file"
 
+
+# --- Status Messagess ---
+echo "Starting .sh file"
 echo "SLURM Job ID: $SLURM_JOB_ID"
 echo "Log file: outputs/logs/train_${SLURM_JOB_ID}.log"
 
-#Activate conda or virtualenv here
+
+
+# --- Load Conda Envrionment ---
+#unload existing modules to avoid conflicts
 module purge
 
-#Use hook + absolute path
-#eval "$(/home/qlh2976/miniconda/bin/conda shell.bash hook)"
-#conda activate /home/qlh2976/miniconda/envs/moodgen"""
+#activate the desired Conda envrionment for training 
 source $(conda info --base)/etc/profile.d/conda.sh
 conda activate moodgen
 
 
-#cd /home/qlh2976/spring2025/genAI/aesthetic_moodboard_generation
+
+# --- Set working directory to job submission location ---
 cd $SLURM_SUBMIT_DIR
+
+
+
+# --- Set PYTHONPATH so that local modules can be imported ---
+export PYTHONPATH=$(pwd)
+
+
 
 echo "Environment activated. Starting training..."
 
-#Make sure current directory is treated as module root
-export PYTHONPATH=$(pwd)
 
-#Run training script
+
+# --- Launch the Training Script ---
 echo "Starting Python training script..."
 python scripts/train_cvae.py
 
-#End
+
+
+# --- Completion Message ---
 echo "Training complete!"
